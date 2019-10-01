9354d61cda179d48a708e21ae469f002dd9bf85f
JerryOsorio28 <jerryOsorio28@users.noreply.github.com>
2019-10-01 12:36:24 -0400

    started card styling

--- react-redux-app/src/components/Series/SuperMarioBros.js
+++ react-redux-app/src/components/Series/SuperMarioBros.js
@@ -21,16 +21,18 @@ const SuperMarioBros = props => {
     return (
         <>
             <button onClick={dataSorter}>testing</button>
-                {array.map(amiibo => (
-                    <div className='amiiboCard'>
-                        <h2>{amiibo.character}</h2>
-                        <img src={amiibo.image} />
-                        <h3>Amiibo Series: {amiibo.amiiboSeries}</h3>
-                        <p>Game Series: {amiibo.gameSeries}</p>
-                        <p>Type: {amiibo.type}</p>
-                    </div>
+                <div className='cardContainer'>
+                    {array.map(amiibo => (
+                        <div className='amiiboCard'>
+                            <h1>{amiibo.character}</h1>
+                            <img src={amiibo.image} />
+                            <h4>Amiibo Series: {amiibo.amiiboSeries}</h4>
+                            <p>Game Series: {amiibo.gameSeries}</p>
+                            <p>Type: {amiibo.type}</p>
+                        </div>
+                    ))}
+                </div>
         
-                ))}
         </>
     )
 }
