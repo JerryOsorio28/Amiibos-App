9125f19dec4775d81d68afba877b4a6f55744a9c
JerryOsorio28 <jerryOsorio28@users.noreply.github.com>
2019-10-01 16:57:32 -0400

    Added title

--- react-redux-app/src/components/Series/SuperMarioBros.js
+++ react-redux-app/src/components/Series/SuperMarioBros.js
@@ -20,18 +20,24 @@ const SuperMarioBros = props => {
 
     return (
         <>
-            <button onClick={dataSorter}>testing</button>
-                <div className='cardContainer'>
-                    {array.map(amiibo => (
-                        <div className='amiiboCard'>
-                            <h1>{amiibo.character}</h1>
-                            <img src={amiibo.image} />
-                            <h4>Amiibo Series: {amiibo.amiiboSeries}</h4>
-                            <p>Game Series: {amiibo.gameSeries}</p>
-                            <p>Type: {amiibo.type}</p>
-                        </div>
-                    ))}
+            <div className='categoryHeader'>
+                <img id='cardTitle' onClick={dataSorter} src ='https://upload.wikimedia.org/wikipedia/commons/thumb/0/05/Mario_Series_Logo.svg/1200px-Mario_Series_Logo.svg.png'/>
+                <div id='pressMe'>
+                    <img src='https://pngriver.com/wp-content/uploads/2018/04/Download-Left-Arrow-PNG-HD.png' alt=''/>
+                    <h1>Press Me!</h1>
                 </div>
+            </div>
+            <div className='cardContainer'>
+                {array.map(amiibo => (
+                    <div className='amiiboCard'>
+                        <h1>{amiibo.character}</h1>
+                        <img src={amiibo.image} />
+                        <h4>Amiibo Series: {amiibo.amiiboSeries}</h4>
+                        <p>Game Series: {amiibo.gameSeries}</p>
+                        <p>Type: {amiibo.type}</p>
+                    </div>
+                ))}
+            </div>
         
         </>
     )
