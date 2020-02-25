0e1bb5a0c833ee813bbcadefb8b9d67f3889b79e
JerryOsorio28 <jerryOsorio28@users.noreply.github.com>
2020-02-25 16:34:04 -0500

    finished home page, looking so much better

--- react-redux-app/src/components/Home.js
+++ react-redux-app/src/components/Home.js
@@ -4,6 +4,15 @@ import React from'react';
 import MarioLife from './Series/images/Home/mario-life.png'
 import Background from './Series/images/Home/amiibo.png'
 import Mario from './Series/images/Home/amiibo-mario.png'
+import Pikmin from './Series/images/Home/amiibo-pikmin.png'
+import Samus from './Series/images/Home/amiibo-samus.png'
+import Sonic from './Series/images/Home/amiibo-sonic.png'
+import Pokeball from './Series/images/Home/amiibo-pokeball.webp'
+import Clouds from './Series/images/Home/clouds.png'
+import Link from './Series/images/Home/amiibo-link.png'
+import Zelda from './Series/images/Home/amiibo-zelda.png'
+import Tails from './Series/images/Home/amiibo-tails.png'
+import Ganon from './Series/images/Home/amiibo-ganon.png'
 
 //STYLES
 import './Home.scss'
@@ -17,23 +26,22 @@ const Home = (props) => {
 
     return (
         <div className='homeContainer'>
+            <p id='homeDescription'>A web app that renders beautifully a collection of Amiibos!</p>
             <img src={Background} id='background'/>
-            <img 
-                src={MarioLife} 
-                alt=''
-                id='life'
-            />
-            <img 
-                src={MarioLife} 
-                alt=''
-                id='smallerLife'
-            />
-            <img 
-                src={Mario} 
-                alt=''
-                id='mario'
-            />
-            {/* <button onClick={clickHandler}>Open Collection</button> */}
+            <img src={MarioLife} alt='' id='life'/>
+            <img src={MarioLife} alt='' id='smallerLife'/>
+            {/* <img src={MarioLife} alt='' id='smallestLife'/> */}
+            <img src={Clouds} alt='' id='clouds'/>
+            <img src={Mario} alt='' id='mario'/>
+            <img src={Pikmin} alt='' id='pikmin'/>
+            <img src={Samus} alt='' id='samus'/>
+            <img src={Sonic} alt='' id='sonic'/>
+            <img src={Tails} alt='' id='tails'/>
+            <img src={Pokeball} alt='' id='pokeball'/>
+            <img src={Link} alt='' id='link'/>
+            <img src={Zelda} alt='' id='zelda'/>
+            <img src={Ganon} alt='' id='ganon'/>
+            <button onClick={clickHandler}>Get started!</button>
         </div>
     )
 };
