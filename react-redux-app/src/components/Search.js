08b5120f98728290a5f4fa1be88d3e97cf1a7e4e
JerryOsorio28 <jerryOsorio28@users.noreply.github.com>
2020-02-27 14:21:34 -0500

    finished flip design

--- react-redux-app/src/components/Search.js
+++ react-redux-app/src/components/Search.js
@@ -47,20 +47,21 @@ const Search = props => {
                 {filteredAmiibos === null ? null : filteredAmiibos.map(amiibo => (
                     <div class="flip-card">
                         <div class="flip-card-inner">
-                            <div class="flip-card-front">
-                                <div className='amiiboCard'>
-                                    <h1>{amiibo.character}</h1>
-                                    <img src={amiibo.image} />
-                                    <h4>Amiibo Series: {amiibo.amiiboSeries}</h4>
-                                    <p>Game Series: {amiibo.gameSeries}</p>
-                                    <p>Type: {amiibo.type}</p>
-                                    {/* <p>Release Dates: {amiibo.release.au}</p> */}
-                                </div>
+                            <div className='amiiboCard'>
+                                <h1 id='cardTitle'>{amiibo.character}</h1>
+                                <img src={amiibo.image} id='amiiboImage'/>
+                                <p id='amiiboSeries'>Amiibo Series: {amiibo.amiiboSeries}</p>
                             </div>
                             <div class="flip-card-back">
-                                <h1>John Doe</h1>
-                                <p>Architect & Engineer</p>
-                                <p>We love that guy</p>
+                                <div id='amiibosDates'>
+                                    <p style={{textAlign: 'left', paddingLeft: '10px'}} >Release Dates:</p>
+                                    <p>Au: {amiibo.release.au}</p>
+                                    <p>Eu: {amiibo.release.eu}</p>
+                                    <p>Jp: {amiibo.release.jp}</p>
+                                    <p>Na: {amiibo.release.na}</p>
+                                </div>
+                                <p style={{margin: '30px 0px 0px'}}>Game Series: {amiibo.gameSeries}</p>
+                                <p style={{marginTop: '10px 0px 0px'}}>Type: {amiibo.type}</p>
                             </div>
                         </div>
                     </div>
